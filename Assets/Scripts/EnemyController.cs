0eb0f440639b508b453411650a2c7a435b51ca06
Huju Lauri <s2000949@edu.bc.fi>
2022-11-04 13:56:17 +0200

    Added enemy patrol paths and fixed enemy bugs - Enemies don't skip patrol points randomly - Enemies don't go through glass walls - Added occlusion culling

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -208,7 +208,7 @@ public class EnemyController : MonoBehaviour
                 patrolTurnTarget = null;
             }
         }
-        else if (agent.remainingDistance < agent.stoppingDistance + 0.5)
+        else if (agent.remainingDistance < agent.stoppingDistance + 0.5 && !agent.pathPending)
         {
             NextPatrolPoint();
         }
