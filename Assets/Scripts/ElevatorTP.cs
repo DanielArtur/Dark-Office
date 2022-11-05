f786c6a3fb480cc45b5d65f3354ae0e63e77e272
Huju Lauri <s2000949@edu.bc.fi>
2022-11-05 11:53:54 +0200

    Fixed elevator positioning and changed wait time

--- Assets/Scripts/ElevatorTP.cs
+++ Assets/Scripts/ElevatorTP.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class ElevatorTP : MonoBehaviour
 {
-    //[SerializeField] GameObject elevatorCabinFloor;
+    [Header("Player")]
     [SerializeField] GameObject playerTransform;
+
+    [Header("Animators")]
     [SerializeField] Animator upperFloorAnimator;
     [SerializeField] Animator currentFloorAnimator;
-    [SerializeField] float tpHeight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //GetComponent<Animator>();
-    }
+    [Header("Teleportation")]
+    [SerializeField] float tpHeight;
+    [SerializeField] float tpWaitTime = 2.5f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player"))
             return;
         StartCoroutine(TPWaitTime());
-        Debug.Log("CheckTP");
     }
 
     private IEnumerator TPWaitTime()
     {
         currentFloorAnimator.SetTrigger("Close");
-        yield return new WaitForSeconds(2);
 
-        //ElevatorTP
+
+        yield return new WaitForSeconds(tpWaitTime);
+
 
         playerTransform.transform.position = new Vector3(playerTransform.transform.position.x, tpHeight, playerTransform.transform.position.z);
-        //elevatorCabinFloor.transform.position.y
 
         
-
-        // Siis help me miten tähän saa animaation liitettyä
         upperFloorAnimator.SetTrigger("Open");
-
-
-        //yield return Elevator.elevatorAnimator.SetBool("canOpen", true);
 
     }
 }
